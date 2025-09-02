27caa9937d80063d945f7cb464c5532a21ce2625
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-09-02 16:01:12 +0200

    Refactor topology.py to improve algorithm flexibility and update .gitignore for vscode. Now the sim_15_nodes executes the simulations in parallel

--- sim_15_nodes_net.sh
+++ sim_15_nodes_net.sh
@@ -1,5 +1,9 @@
-echo "Low load simulation on 15 nodes graph"
-python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.01
+# echo "Low load simulation on 15 nodes graph"
+# python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.01
 
-echo "Heavy load simulation on 15 nodes graph"
-python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.002
+# echo "Heavy load simulation on 15 nodes graph"
+# python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.002
+
+python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.01 &
+python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.002 &
+wait
