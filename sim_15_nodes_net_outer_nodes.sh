8f16fbe86271d3769b1363595a41ca6ab60a4f4a
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-09-14 00:28:58 +0200

    Refactor simulation script to run sequentially and comment out parallel execution

--- sim_15_nodes_net_outer_nodes.sh
+++ sim_15_nodes_net_outer_nodes.sh
@@ -1,8 +1,8 @@
 TRAFFIC_FILE="project/file/traffic_15_nodes_outer_chatting.json"
-python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm betweenness &
-python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm betweenness &
-python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm shortest &
-python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm shortest &
-python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm greedy &
-python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm greedy &
-wait
+python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm betweenness
+# python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm betweenness &
+python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm shortest
+# python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm shortest &
+python3 project/sim_ext.py --sim-time 7.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.01 --algorithm greedy
+# python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic "$TRAFFIC_FILE" --inspection-rate 0.001 --mess-rate 0.002 --algorithm greedy &
+# wait
