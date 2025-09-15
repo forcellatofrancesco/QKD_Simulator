68738f9beb2c97652e89b3fc855b393bc1e455d2
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-09-15 17:14:21 +0200

    Implement traffic generation for random node communication and update output file structure

--- project/gen_traffic.py
+++ project/gen_traffic.py
@@ -1,8 +1,8 @@
 import json
+import random
 
 
-def main():
-    output_file: str = "./project/file/traffic_15_nodes_outer_chatting.json"
+def gen_outer_traffic():
     outer_nodes: list[int] = [7, 8, 9, 10, 11, 12, 13, 14]
     # inner_nodes: list[int] = [0, 1, 2, 3, 4, 5, 6]
     packets: list[dict] = []
@@ -18,6 +18,31 @@ def main():
         )
         packet = {"src_node": f"node{node}", "dst_node": dst_nodes}
         packets.append(packet)
+
+
+def gen_random_traffic():
+    length = 15
+    nodes = list(range(0, length))
+    packets: list[dict] = []
+    for node in nodes:
+        dst_nodes: list[int] = []
+        while len(dst_nodes) < length // 2:
+            r = random.randint(0, length - 1)
+            if r != node and r not in dst_nodes:
+                dst_nodes.append(r)
+
+        packet = {
+            "src_node": f"node{node}",
+            "dst_node": list(map(lambda x: f"node{x}", sorted(dst_nodes))),
+        }
+
+        packets.append(packet)
+    return packets
+
+
+def main():
+    output_file: str = "./project/file/traffic_15_nodes_random.json"
+    packets = gen_random_traffic()
     traffic = {"packets": packets}
     with open(output_file, "w") as f:
         json.dump(traffic, f, indent=4)
