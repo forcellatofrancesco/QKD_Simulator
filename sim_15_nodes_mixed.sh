c323d84cf8cc88faca1a7abc3e8a22a860e195ab
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-09-17 10:03:14 +0200

    Add missing sleep command for heavy load simulation

--- sim_15_nodes_mixed.sh
+++ sim_15_nodes_mixed.sh
@@ -1,5 +1,6 @@
 echo "Heavy load simulation on 15 nodes graph"
 python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_net.json --inspection-rate 0.001 --mess-rate 0.002 &
+sleep 2
 python3 project/sim_ext.py --sim-time 1.5 --seq-graph project/file/graph_15_nodes.json --traffic project/file/traffic_15_nodes_random.json --inspection-rate 0.001 --mess-rate 0.002 &
 wait
 
